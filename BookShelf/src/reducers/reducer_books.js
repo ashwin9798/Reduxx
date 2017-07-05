a9f66028e0a08cc00ccc6b3087924176335e7c55
Ashwin Vivek <ashwin9798@gmail.com>
2017-07-05 23:16:11 +0800

    first miniproject complete

---
+++ BookShelf/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function()
+{
+  return [
+    {title: 'Harry Potter', pages: 403},
+    {title: 'JAVA, the language', pages: 40},
+    {title: 'The Dark Tower', pages: 133},
+    {title: 'Shutter Island', pages: 456}
+  ]
+}
--- src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-export default function()
-{
-  return [
-    {title: 'Harry Potter'},
-    {title: 'JAVA, the language'},
-    {title: 'The Dark Tower'},
-    {title: 'Shutter Island'}
-  ]
-}
