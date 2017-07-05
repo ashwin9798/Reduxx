c68b6cea7a4a8f0822566dac741eba3e23d3918e
Ashwin Vivek <ashwin9798@gmail.com>
2017-07-05 22:58:01 +0800

    book detail

--- src/reducers/reducer_active_books.js
+++ src/reducers/reducer_active_books.js
@@ -1,6 +1,6 @@
 //state argument is state that reducer is
 //responsible for
-export default function(state, action) {
+export default function(state=null, action) {
   switch(action.type) {
     case 'BOOK_SELECTED':
       return action.payload;
