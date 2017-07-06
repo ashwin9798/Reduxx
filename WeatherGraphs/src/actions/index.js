997d568e775ed469fcf51b9589a6c5d37a53cdc9
Ashwin Vivek <ashwin9798@gmail.com>
2017-07-06 23:04:26 +0800

    API request using axios

--- WeatherGraphs/src/actions/index.js
+++ WeatherGraphs/src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = '91261f4534decd7c41d0cb5b8a206778'
+import axios from 'axios';
+
+const API_KEY = '91261f4534decd7c41d0cb5b8a206778';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
